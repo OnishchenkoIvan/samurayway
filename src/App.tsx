ccee34b3a1d839b1b41243f63e076b4a535fd774
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-25 22:19:43 +0300

    08 start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Technologies />
+    </div>
+  );
+}
 
+const Technologies = () => {
+  return (
+    <div>
+      <ul>
+        <li>css</li>
+        <li>html</li>
+        <li>react</li>
+        <li>js</li>
+      </ul>
+      ;
+    </div>
+  );
+};
+
+function Header() {
+  return (
+    <div className="App">
+      <a href="#s">Home</a>
+      <a href="#s">News</a>
+      <a href="#s">Messages</a>
+      <a href="#s">Conacts</a>
     </div>
   );
 }
