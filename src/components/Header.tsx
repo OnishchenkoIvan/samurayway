5eb8bbfda3a1d0a5390760c45e57869d8eb9e02c
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-26 19:28:59 +0300

    14 css-modules

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import s from "./Header.module.css";
 
 export const Header = () => {
   return (
-    <header className="header">
-      <img src="https://i.pinimg.com/originals/7f/c0/0d/7fc00d890352bfeea2239b8bfced0481.jpg" />
+    <header className={s.header}>
+      <img
+        src="https://i.pinimg.com/originals/7f/c0/0d/7fc00d890352bfeea2239b8bfced0481.jpg"
+        alt={"avatar"}
+      />
     </header>
   );
 };
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,23 @@
 import React from "react";
+import s from "./Profile.module.css";
 
 export const Profile = () => {
   return (
-    <div className="content">
+    <div className={s.content}>
       <div>
-        <img src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?cs=srgb&dl=pexels-fabian-wiktor-994605.jpg&fm=jpg" />
+        <img
+          src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?cs=srgb&dl=pexels-fabian-wiktor-994605.jpg&fm=jpg"
+          alt={"beach"}
+        />
       </div>
       <div>ava + description</div>
       <div>
         my posts
         <div>new post</div>
-        <div>post 1</div>
-        <div>post 2</div>
+        <div className={s.posts}>
+          <div className={s.item}>post 1</div>
+          <div className={s.item}>post 2</div>
+        </div>
       </div>
     </div>
   );
