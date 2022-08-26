16fefaad7e562b9e4d4b2006d1bf9c11a6a736d6
Ivan Onishchenko <Lightsout07@mail.ru>
2022-08-26 14:57:32 +0300

    12 components start

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,41 @@
 import React from "react";
 import "./App.css";
-import { Technologies } from "./Technologies";
-import { Header } from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Technologies />
+    <div className="app-wrapper">
+      <header className="header">
+        <img src="https://i.pinimg.com/originals/7f/c0/0d/7fc00d890352bfeea2239b8bfced0481.jpg" />
+      </header>
+      <nav className="nav">
+        <div>
+          <a>Profile</a>
+        </div>
+        <div>
+          <a>Messages</a>
+        </div>
+        <div>
+          <a>News</a>
+        </div>
+        <div>
+          <a>Music</a>
+        </div>
+        <div>
+          <a>Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img src="https://images.pexels.com/photos/994605/pexels-photo-994605.jpeg?cs=srgb&dl=pexels-fabian-wiktor-994605.jpg&fm=jpg" />
+        </div>
+        <div>ava + description</div>
+        <div>
+          my posts
+          <div>new post</div>
+          <div>post 1</div>
+          <div>post 2</div>
+        </div>
+      </div>
     </div>
   );
 }
